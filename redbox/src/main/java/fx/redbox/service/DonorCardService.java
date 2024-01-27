5161f82592224ae13c229092c267eb80dcac4e0b
02yebin <agabin0214@naver.com>
2024-01-27 12:43:34 +0900

    FX-41 feat: [BE] 헌혈증 관리 service 생성
    FX-41 feat: [BE] 헌혈증 관리 service 생성
    
    -헌혈증 등록, 찾기, 수정, 삭제 인터페이스 생성

---
+++ redbox/src/main/java/fx/redbox/service/DonorCardService.java
@@ -0,0 +1,13 @@
+package fx.redbox.service;
+
+import fx.redbox.entity.donorCards.DonorCard;
+
+import java.util.List;
+import java.util.Map;
+
+public interface DonorCardService {
+    void join(DonorCard donorCard);
+    List<Map<String, Object>> findDonorCard(String certificateNumber);
+    void deleteDonorCard(String certificateNumber);
+    void updateDonorCard(String certificateNumber, DonorCard updateDonorCard);
+}
