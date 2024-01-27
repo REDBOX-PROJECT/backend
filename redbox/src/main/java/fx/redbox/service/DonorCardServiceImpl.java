4ef8b2887acc7d3e381e11e73efeb201833b4f12
02yebin <agabin0214@naver.com>
2024-01-27 12:46:10 +0900

    FX-41 feat: [BE] 헌혈증 관리 service 구현
    FX-41 feat: [BE] 헌혈증 관리 service 구현
    
    -@RequireArgsConstructor 어노테이션을 이용하여 생성자 없이 donorCardRepository를 자동 등록 시켜줌

---
+++ redbox/src/main/java/fx/redbox/service/DonorCardServiceImpl.java
@@ -0,0 +1,15 @@
+package fx.redbox.service;
+
+import fx.redbox.entity.donorCards.DonorCard;
+import fx.redbox.repository.DonorCardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class DonorCardServiceImpl implements DonorCardService{
+    private final DonorCardRepository donorCardRepository;
+}
