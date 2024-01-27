d190a173703dca9864e2a7ae48489c0a76008e01
02yebin <agabin0214@naver.com>
2024-01-27 12:21:43 +0900

    FX-45 feat: [BE] Builder 패턴 적용
    FX-45 feat: [BE] Builder 패턴 적용

--- redbox/src/main/java/fx/redbox/entity/donorCards/DonorCard.java
+++ redbox/src/main/java/fx/redbox/entity/donorCards/DonorCard.java
@@ -2,6 +2,7 @@ package fx.redbox.entity.donorCards;
 
 import fx.redbox.entity.enums.DonorBloodKind;
 import fx.redbox.entity.enums.Gender;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,4 +18,15 @@ public class DonorCard {
     private Gender donorGender;
     private String bloodCenter;
     private Long userId;
+
+    @Builder
+    public DonorCard(String certificateNumber, String donorName, Date donorBirth, DonorBloodKind donorBloodKind, Gender donorGender, String bloodCenter, Long userId){
+        this.certificateNumber = certificateNumber;
+        this.donorName = donorName;
+        this.donorBirth = donorBirth;
+        this.donorBloodKind = donorBloodKind;
+        this.donorGender = donorGender;
+        this.bloodCenter = bloodCenter;
+        this.userId = userId;
+    }
 }
