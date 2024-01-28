11a4ab564fc40d23c57b27608508c60b69a97977
SEOB <67796853+seob7@users.noreply.github.com>
2024-01-28 20:03:04 +0900

    FX-44 refactor: [BE] 사용자 등록 Form

---
+++ redbox/src/main/java/fx/redbox/controller/user/form/UserForm.java
@@ -0,0 +1,27 @@
+package fx.redbox.controller.user.form;
+
+import fx.redbox.entity.enums.BloodType;
+import fx.redbox.entity.enums.Gender;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+public class UserForm {
+
+    //User
+    private String name;
+    private Date birth; //YYYY-MM-DD
+    private Gender gender;
+    private BloodType bloodType;
+
+    //UserAccount
+    private String email;
+    private String password;
+
+    //UserInfo
+    private String phone;
+    private String address;
+}
