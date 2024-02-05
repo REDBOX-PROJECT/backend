64a20ffd6dc89b90880c2803e641d45088e60937
SEOB <67796853+seob7@users.noreply.github.com>
2024-02-05 14:06:38 +0900

    FX-44 refactor: [BE] 사용자 등록 Repository 메서드 분리

--- redbox/src/main/java/fx/redbox/repository/User/UserRepository.java
+++ redbox/src/main/java/fx/redbox/repository/User/UserRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
+
     User save (UserAccount userAccount, UserInfo userInfo, User user);
 
     Optional<User> findByUserId(Long userId);
--- redbox/src/main/java/fx/redbox/repository/User/UserRepositoryImpl.java
+++ redbox/src/main/java/fx/redbox/repository/User/UserRepositoryImpl.java
@@ -2,7 +2,6 @@ package fx.redbox.repository.user;
 
 
 import fx.redbox.entity.enums.Grade;
-import fx.redbox.entity.enums.Permission;
 import fx.redbox.entity.users.User;
 import fx.redbox.entity.users.UserAccount;
 import fx.redbox.entity.users.UserInfo;
@@ -27,45 +26,10 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User save(UserAccount userAccount, UserInfo userInfo, User user) {
-
-        // user_accounts 테이블
-        SimpleJdbcInsert userAccountJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("user_accounts")
-                .usingGeneratedKeyColumns("account_id");
-        Map<String, Object> userAccountParam = new ConcurrentHashMap<>();
-        userAccountParam.put("email", userAccount.getEmail());
-        userAccountParam.put("password", userAccount.getPassword());
-//        user.setAccountId(userAccountJdbcInsert.executeAndReturnKey(userAccountParam).longValue());
-
-
-        // user_info 테이블
-        SimpleJdbcInsert userInfoJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("user_info")
-                .usingGeneratedKeyColumns("user_info_id");
-        Map<String, Object> userInfoParam = new ConcurrentHashMap<>();
-        userInfoParam.put("phone", userInfo.getPhone());
-        userInfoParam.put("address", userInfo.getAddress());
-        userInfoParam.put("donation_count", 0); //DEFAULT 0
-        userInfoParam.put("permission", userInfo.getPermission().name()); //DEFAULT USER
-//        user.setUserInfoId(userInfoJdbcInsert.executeAndReturnKey(userInfoParam).longValue());
-
-
-        // users 테이블
-        SimpleJdbcInsert userJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("users")
-                .usingGeneratedKeyColumns("user_id");
-        Map<String, Object> userParam = new ConcurrentHashMap<>();
-        userParam.put("name", user.getName());
-        userParam.put("birth", user.getBirth());
-        userParam.put("gender", user.getGender().name());
-        userParam.put("blood_type", user.getBloodType().name());
-        userParam.put("grade", Grade.BASIC.name());
-        userParam.put("account_id", userAccountJdbcInsert.executeAndReturnKey(userAccountParam).longValue());
-        userParam.put("user_info_id", userInfoJdbcInsert.executeAndReturnKey(userInfoParam).longValue());
-        userJdbcInsert.executeAndReturnKey(userParam).longValue();
-
+        saveUserData(userAccount, userInfo, user);
         return user;
     }
+
 
     @Override
     public Optional<User> findByUserId(Long userId) {
@@ -143,5 +107,44 @@ public class UserRepositoryImpl implements UserRepository {
                 " WHERE email = ?";
         jdbcTemplate.update(sql, password, email);
     }
+
+
+
+
+
+    private void saveUserData(UserAccount userAccount, UserInfo userInfo, User user) {
+        // user_accounts 테이블
+        SimpleJdbcInsert userAccountJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("user_accounts")
+                .usingGeneratedKeyColumns("account_id");
+        Map<String, Object> userAccountParam = new ConcurrentHashMap<>();
+        userAccountParam.put("email", userAccount.getEmail());
+        userAccountParam.put("password", userAccount.getPassword());
+
+        // user_info 테이블
+        SimpleJdbcInsert userInfoJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("user_info")
+                .usingGeneratedKeyColumns("user_info_id");
+        Map<String, Object> userInfoParam = new ConcurrentHashMap<>();
+        userInfoParam.put("phone", userInfo.getPhone());
+        userInfoParam.put("address", userInfo.getAddress());
+        userInfoParam.put("donation_count", 0); //DEFAULT 0
+        userInfoParam.put("permission", userInfo.getPermission().name()); //DEFAULT USER
+
+        // users 테이블
+        SimpleJdbcInsert userJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("users")
+                .usingGeneratedKeyColumns("user_id") ;
+        Map<String, Object> userParam = new ConcurrentHashMap<>();
+        userParam.put("name", user.getName());
+        userParam.put("birth", user.getBirth());
+        userParam.put("gender", user.getGender().name());
+        userParam.put("blood_type", user.getBloodType().name());
+        userParam.put("grade", Grade.BASIC.name());
+        userParam.put("account_id", userAccountJdbcInsert.executeAndReturnKey(userAccountParam).longValue());
+        userParam.put("user_info_id", userInfoJdbcInsert.executeAndReturnKey(userInfoParam).longValue());
+        userJdbcInsert.executeAndReturnKey(userParam).longValue();
+    }
+
 
 }
