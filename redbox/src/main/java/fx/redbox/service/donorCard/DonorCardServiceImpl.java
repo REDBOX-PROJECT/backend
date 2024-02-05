d9e3c45407f5791c497899487b7e52aca36f2725
02yebin <agabin0214@naver.com>
2024-02-05 14:08:32 +0900

    FX-45 perf: [BE] 헌혈증 정보 등록 service 기능 수정
    FX-45 perf: [BE] 헌혈증 정보 등록 service 기능 수정
    
    -메서드 이름 수정
    -조건 추가
    -반환 타입 수정

--- redbox/src/main/java/fx/redbox/service/donorCard/DonorCardServiceImpl.java
+++ redbox/src/main/java/fx/redbox/service/donorCard/DonorCardServiceImpl.java
@@ -5,16 +5,27 @@ import fx.redbox.repository.donorCard.DonorCardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class DonorCardServiceImpl implements DonorCardService{
+
     private final DonorCardRepository donorCardRepository;
+
     @Override
-    public void join(DonorCard donorCard) {
-        donorCardRepository.save(donorCard);
+    public Optional<DonorCard> saveDonorCard(DonorCard donorCard) throws SQLException {
+
+        Optional<DonorCard> findDonorCard = donorCardRepository.findDonorCardByCertificateNumber(donorCard.getCertificateNumber());
+        if (!findDonorCard.isEmpty()){
+            return findDonorCard; // 등록하려는 헌혈증이 이미 있다면 그 헌혈증의 정보 반환
+        }
+        
+        Optional<DonorCard> savedDonorCard = donorCardRepository.saveDonorCard(donorCard);
+        
+        return savedDonorCard;
     }
 
     @Override
