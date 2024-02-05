276be1e76e92aa0ef3a432fc6212374af9e086f5
02yebin <agabin0214@naver.com>
2024-02-05 13:55:18 +0900

    FX-41 perf: [BE] 헌혈증 관리 repository 수정
    FX-41 perf: [BE] 헌혈증 관리 repository 수정
    
    -메서드 이름 수정(짐작하기 쉽도록)
    -반환타입 수정

--- redbox/src/main/java/fx/redbox/repository/donorCard/DonorCardRepository.java
+++ redbox/src/main/java/fx/redbox/repository/donorCard/DonorCardRepository.java
@@ -2,13 +2,20 @@ package fx.redbox.repository.donorCard;
 
 import fx.redbox.entity.donorCards.DonorCard;
 
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 public interface DonorCardRepository {
-    void save(DonorCard donorCard);
-    List<Map<String, Object>> findById(String certificateNumber);
-    List<Map<String, Object>> findAll();
-    void update(String certificateNumber, DonorCard updateDonorCard);
-    void delete(String certificateNumber);
+
+    Optional<DonorCard> saveDonorCard(DonorCard donorCard) throws SQLException;
+
+    Optional<DonorCard> findDonorCardByCertificateNumber(String certificateNumber) throws SQLException;
+
+    List<DonorCard> findAllDonorCards() throws SQLException;
+
+    void updateDonorCard(String certificateNumber, DonorCard updateDonorCard) throws SQLException;
+
+    void deleteDonorCard(String certificateNumber) throws SQLException;
+
 }
