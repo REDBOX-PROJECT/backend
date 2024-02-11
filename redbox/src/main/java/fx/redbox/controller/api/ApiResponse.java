77a3a2ee5683e0a69dee9cbe6b4d34ffd8c3388d
Jhynguu <jhyngu0531@gmail.com>
2024-02-11 12:04:41 +0900

    FX-43 perf: [BE] Controller 수정

--- redbox/src/main/java/fx/redbox/controller/api/ApiResponse.java
+++ redbox/src/main/java/fx/redbox/controller/api/ApiResponse.java
@@ -14,7 +14,57 @@ public class ApiResponse<T> {
     private String responseMessage;
     private T data;
 
+    public ApiResponse(final int statusCode, final String responseMessage, final T data) {
+        this.statusCode = statusCode;
+        this.responseMessage = responseMessage;
+        this.data = data;
+    }
+
+    /*
+    '짐작한대로 작동한다면 잘 작성된 코드이다.',
+    메서드는 한 추상화 단계만 진행할 것
+    add -> 더하다 , 추가하다.
+    add(){
+        // 데이터 초기화 2
+         // 데이터 검증 2
+        // 더하기 추상화 1단계
+        // 반환
+    }
+     */
+    public static<T> ApiResponse<T> fail(final int statusCode, final String responseMessage) {
+        return ApiResponse.<T>builder()
+                .statusCode(statusCode)
+                .responseMessage(responseMessage)
+                .build();
+    }
+
+    public static<T> ApiResponse<T> success(final String responseMessage, final T data) {
+        return ApiResponse.<T>builder()
+                .data(data)
+                .statusCode(StatusCode.OK) // success 는 기본으로 StatusCode.OK 를 가진다.
+                .responseMessage(responseMessage)
+                .build();
+    }
+}
+
+
+/*
+package fx.redbox.controller.api;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+public class ApiResponse<T> {
+
+    private int statusCode;
+    private String responseMessage;
+    private T data;
+
     public ApiResponse(int statusCode, String responseMessage, T data) {
+
         this.statusCode = statusCode;
         this.responseMessage = responseMessage;
         this.data = data;
@@ -24,6 +74,11 @@ public class ApiResponse<T> {
         return res(statusCode, responseMessage, null);
     }
 
+    public static<T> ApiResponse resOk(final String responseMessage) {
+        return res(HttpStatus.OK.value(), responseMessage, null);
+    }
+
+
     public static<T> ApiResponse<T> res(final int statusCode, final String responseMessage, final T t) {
         return ApiResponse.<T>builder()
                 .data(t)
@@ -31,4 +86,13 @@ public class ApiResponse<T> {
                 .responseMessage(responseMessage)
                 .build();
     }
+
+    public static<T> ApiResponse<T> resOK(final String responseMessage, final T t) {
+        return ApiResponse.<T>builder()
+                .data(t)
+                .statusCode(HttpStatus.OK.value())
+                .responseMessage(responseMessage)
+                .build();
+    }
 }
+*/
--- redbox/src/main/java/fx/redbox/controller/donorCard/DonorCardRequestDto.java
+++ redbox/src/main/java/fx/redbox/controller/donorCard/DonorCardRequestDto.java
@@ -1,5 +1,6 @@
 package fx.redbox.controller.donorCard;
 
+import fx.redbox.entity.donorCards.DonorCardRequest;
 import fx.redbox.entity.donorCards.DonorCardRequestForm;
 import fx.redbox.entity.enums.BloodType;
 import fx.redbox.entity.enums.DonorCardRequestRejectReason;
@@ -13,17 +14,8 @@ import java.sql.Timestamp;
 @Getter
 @Setter
 public class DonorCardRequestDto {
-
-    // DonorCardRequest
-    private RejectPermission donorCardRequestPermission;
-    private DonorCardRequestRejectReason donorCardRequestRejectReason;
-    private Long userId;
+    private DonorCardRequest donorCardRequest;
     private DonorCardRequestForm donorCardRequestForm;
 
-    // DonorCardRequestForm
-    private String patientName;
-    private String evidenceDocument;
-    private Gender patientGender;
-    private BloodType bloodType;
-    private Timestamp donorCardRequestDate;
 }
+
