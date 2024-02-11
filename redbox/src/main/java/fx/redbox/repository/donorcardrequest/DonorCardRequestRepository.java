271d926424b98ba1ee113dff7d8d9d1b31affce4
Jhynguu <jhyngu0531@gmail.com>
2024-02-11 12:24:04 +0900

    FX-43 perf: [BE] Repository 수정

--- redbox/src/main/java/fx/redbox/repository/donorcardrequest/DonorCardRequestRepository.java
+++ redbox/src/main/java/fx/redbox/repository/donorcardrequest/DonorCardRequestRepository.java
@@ -1,21 +1,26 @@
-package fx.redbox.repository.donorcardrequest;
+package fx.redbox.repository.donorCardRequest;
 
 import fx.redbox.entity.donorCards.DonorCardRequest;
 import fx.redbox.entity.donorCards.DonorCardRequestForm;
+import fx.redbox.entity.enums.DonorCardRequestRejectReason;
+import fx.redbox.entity.enums.RejectPermission;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DonorCardRequestRepository {
 
     DonorCardRequest createDonorCardRequest(DonorCardRequest donorCardRequest, DonorCardRequestForm donorCardRequestForm);
 
-    DonorCardRequest getDonorCardRequestById(String donorCardRequestId);
+    Optional<DonorCardRequest> getDonorCardRequestById(Long donorCardRequestId);
 
     List<DonorCardRequest> getAllDonorCardRequests();
 
-    DonorCardRequest updateDonorCardRequest(DonorCardRequest donorCardRequest, DonorCardRequestForm donorCardRequestForm);
+    void updateDonorCardRequest(Long donorCardRequestId, RejectPermission donorCardRequestPermission, DonorCardRequestRejectReason donorCardRequestRejectReason);
 
-    void deleteDonorCardRequest(String donorCardRequestId);
+    void updateDonorCardRequestForm(Long donorCardRequestId, String evidenceDocument);
+
+    void deleteDonorCardRequest(Long donorCardRequestId);
 }
--- redbox/src/main/java/fx/redbox/repository/mappper/DonorCardRequestMapper.java
+++ redbox/src/main/java/fx/redbox/repository/mappper/DonorCardRequestMapper.java
@@ -17,6 +17,14 @@ public class DonorCardRequestMapper implements RowMapper<DonorCardRequest> {
     @Override
     public DonorCardRequest mapRow(ResultSet rs, int rowNum) throws SQLException {
 
+        DonorCardRequest donorCardRequest = DonorCardRequest.builder()
+                .donorCardRequestId(rs.getLong("donorcard_request_id"))
+                .donorCardRequestPermission(RejectPermission.valueOf(rs.getString("donorcard_request_permission")))
+                .donorCardRequestRejectReason(DonorCardRequestRejectReason.valueOf(rs.getString("donorcard_request_reject_reason")))
+                .userId(rs.getLong("user_id"))
+                .donorCardRequestFormId(rs.getLong("donorcard_request_form_id"))
+                .build();
+
         DonorCardRequestForm donorCardRequestForm = DonorCardRequestForm.builder()
                 .donorCardRequestId(rs.getLong("donorcard_request_id"))
                 .patientName(rs.getString("patient_name"))
@@ -25,14 +33,7 @@ public class DonorCardRequestMapper implements RowMapper<DonorCardRequest> {
                 .bloodType(BloodType.valueOf(rs.getString("blood_type")))
                 .donorCardRequestDate(rs.getTimestamp("donorcard_request_date"))
                 .build();
-
-        DonorCardRequest donorCardRequest = DonorCardRequest.builder()
-                .donorCardRequestId(rs.getLong("donorcard_request_id"))
-                .donorCardRequestPermission(RejectPermission.valueOf(rs.getString("donorcard_request_permission")))
-                .donorCardRequestRejectReason(DonorCardRequestRejectReason.valueOf(rs.getString("donorcard_request_reject_reason")))
-                .userId(rs.getLong("user_id"))
-                .donorCardRequestForm(donorCardRequestForm)  // RequestForm 객체 설정
-                .build();
+        donorCardRequest.setDonorCardRequestForm(donorCardRequestForm);
 
         return donorCardRequest;
     }
