22fb2c121d170986f5187ca7f7d89710fa4f1704
Jhynguu <jhyngu0531@gmail.com>
2024-02-11 12:15:19 +0900

    Revert "FX-43 perf: [BE] Controller 수정"
    This reverts commit 77a3a2ee5683e0a69dee9cbe6b4d34ffd8c3388d.

--- redbox/src/main/java/fx/redbox/controller/donorCard/DonorCardRequestDto.java
+++ redbox/src/main/java/fx/redbox/controller/donorCard/DonorCardRequestDto.java
@@ -1,6 +1,5 @@
 package fx.redbox.controller.donorCard;
 
-import fx.redbox.entity.donorCards.DonorCardRequest;
 import fx.redbox.entity.donorCards.DonorCardRequestForm;
 import fx.redbox.entity.enums.BloodType;
 import fx.redbox.entity.enums.DonorCardRequestRejectReason;
@@ -14,8 +13,17 @@ import java.sql.Timestamp;
 @Getter
 @Setter
 public class DonorCardRequestDto {
-    private DonorCardRequest donorCardRequest;
+
+    // DonorCardRequest
+    private RejectPermission donorCardRequestPermission;
+    private DonorCardRequestRejectReason donorCardRequestRejectReason;
+    private Long userId;
     private DonorCardRequestForm donorCardRequestForm;
 
+    // DonorCardRequestForm
+    private String patientName;
+    private String evidenceDocument;
+    private Gender patientGender;
+    private BloodType bloodType;
+    private Timestamp donorCardRequestDate;
 }
-
