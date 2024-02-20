5948434768673c0eb88a6b90017963e5657d6261
SEOB <67796853+seob7@users.noreply.github.com>
2024-02-20 19:28:43 +0900

    FX-40 peat: [BE] Swagger 어노테이션 적용

--- redbox/src/main/java/fx/redbox/controller/user/SignController.java
+++ redbox/src/main/java/fx/redbox/controller/user/SignController.java
@@ -5,6 +5,9 @@ import fx.redbox.controller.api.UserResponseMessage;
 import fx.redbox.controller.user.form.SignInForm;
 import fx.redbox.controller.user.form.SignUpForm;
 import fx.redbox.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Tag(name = "SIGN API", description = "사용자 인증 관리 API")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -22,6 +26,11 @@ public class SignController {
     private final UserService userService;
 
     @PostMapping("/login") //로그인
+    @Operation(summary = "로그인",
+            description = "로그인 API"
+    )
+    @ApiResponse(responseCode = "200", description = "성공")
+    @ApiResponse(responseCode = "400", description = "사용자를 찾을 수 없습니다.")
     public ResponseApi signIn(@RequestBody @Valid SignInForm signInForm) {
         return ResponseApi.success(UserResponseMessage.LOGIN_SUCCESS.getMessage(), userService.signIn(signInForm));
     }
@@ -31,6 +40,11 @@ public class SignController {
 //    @RestControllerAdvice
 
     @PostMapping("/register") //회원가입
+    @Operation(summary = "회원가입",
+            description = "회원가입 API"
+    )
+    @ApiResponse(responseCode = "200", description = "성공")
+    @ApiResponse(responseCode = "400", description = "이미 존재하는 회원입니다.")
     public ResponseApi<Boolean> signUp(@RequestBody @Valid SignUpForm signUpForm) {
         boolean resultBoolean = userService.signUp(signUpForm);
         return ResponseApi.success(UserResponseMessage.CREATED_USER.getMessage(), resultBoolean);
