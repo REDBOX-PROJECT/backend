93352572d94ec6dbc3b907b3d9527f5b54841e9b
SEOB <67796853+seob7@users.noreply.github.com>
2024-02-23 17:23:42 +0900

    FX-71 feat: [BE] swagger 적용

--- redbox/src/main/java/fx/redbox/controller/statistics/StatisticsController.java
+++ redbox/src/main/java/fx/redbox/controller/statistics/StatisticsController.java
@@ -4,12 +4,16 @@ import fx.redbox.controller.api.ResponseApi;
 import fx.redbox.controller.api.StatisticsResponseMessage;
 import fx.redbox.controller.statistics.form.StatisticsForm;
 import fx.redbox.service.statistics.StatisticsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
 
+@Tag(name = "STATISTICS API", description = "통계 관리 API")
 @RestController
 @AllArgsConstructor
 public class StatisticsController {
@@ -17,6 +21,11 @@ public class StatisticsController {
     StatisticsService statisticsService;
 
     @GetMapping("/statistics")
+    @Operation(summary = "헌혈 통계",
+            description = "헌혈종류, 성별, 나이대 통계를 나타냅니다."
+    )
+    @ApiResponse(responseCode = "200", description = "성공")
+    @ApiResponse(responseCode = "400", description = "통계값을 불러올 수 없습니다.")
     public ResponseApi<StatisticsForm> showStatistics() throws SQLException {
         StatisticsForm statisticsForm = statisticsService.showStatistics();
         return ResponseApi.success(StatisticsResponseMessage.STATISTICS_SUCCESS.getMessage(), statisticsForm);
