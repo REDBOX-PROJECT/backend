3c0c982c719dc1d3690de2e2539e9fc67519a4d7
SEOB <67796853+seob7@users.noreply.github.com>
2024-03-02 16:33:42 +0900

    perf: [BE] 예외처리 성능 개선

--- redbox/src/main/java/fx/redbox/controller/board/BoardController.java
+++ redbox/src/main/java/fx/redbox/controller/board/BoardController.java
@@ -1,5 +1,8 @@
 package fx.redbox.controller.board;
 
+import fx.redbox.common.Exception.BoardNotFoundException;
+import fx.redbox.common.Exception.UserNotFoundException;
+import fx.redbox.controller.api.BoardResponseMessage;
 import fx.redbox.controller.api.ResponseApi;
 import fx.redbox.controller.board.form.AllBoardForm;
 import fx.redbox.controller.board.form.BoardForm;
@@ -29,10 +32,13 @@ public class BoardController {
         //테스트 아이디입니다.
         Optional<User> user = userService.findByUserId(1L);
         if(user.isEmpty())
-            return ResponseApi.fail(400, "사용자 없음");
+            throw new UserNotFoundException();
 
         boardService.saveBoard(board, user.get());
-        return ResponseApi.success("게시글(문의 및 공지사항) 저장 성공");
+        if(board.getBoardType().equals(BoardType.문의))
+            return ResponseApi.success(BoardResponseMessage.CREATED_INQUIRY_BOARD.getMessage());
+
+        return ResponseApi.success(BoardResponseMessage.CREATED_NOTICE_BOARD.getMessage());
     }
 
 
@@ -42,19 +48,20 @@ public class BoardController {
         //해당하는 게시글 불러오기
         AllBoardForm board = boardService.getBoard(boardId);
 
-        //해당하는 타입이 문의가 맞으면
-        if (board.getBoardType().equals(BoardType.문의)) {
-            boardService.saveInquiryAnswer(board.getBoardId(), inquiryAnswerForm);
-            return ResponseApi.success("게시글(문의 및 공지사항) 저장 성공");
+        //문의 게시글이 아니면
+        if (!board.getBoardType().equals(BoardType.문의)) {
+            throw new BoardNotFoundException();
         }
 
-        return ResponseApi.fail(400, "문의게시글이 아닙니다.");
+        //문의 게시글이면
+        boardService.saveInquiryAnswer(board.getBoardId(), inquiryAnswerForm);
+        return ResponseApi.success(BoardResponseMessage.CREATED_INQUIRY_BOARD.getMessage());
     }
 
 
     @GetMapping("/inquiry/list")
     public ResponseApi<List<InquiryListForm>> showInquiryList() {
         List<InquiryListForm> inquiryListForm = boardService.showInquiryList();
-        return ResponseApi.success("성공", inquiryListForm);
+        return ResponseApi.success(BoardResponseMessage.SUCCESS_SHOW_INQUIRY_LIST.getMessage(), inquiryListForm);
     }
  }
