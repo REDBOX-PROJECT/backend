13e498f9c2d203b8ca44797d7c11329db479f4c0
Jhynguu <jhyngu0531@gmail.com>
2024-03-22 18:33:32 +0900

    feat: [BE] RedBoxDashboardInfo 클래스 추가

--- redbox/src/main/java/fx/redbox/service/donorCard/DonorCardServiceImpl.java
+++ redbox/src/main/java/fx/redbox/service/donorCard/DonorCardServiceImpl.java
@@ -5,11 +5,11 @@ import fx.redbox.common.Exception.DuplicateCertificateNumberException;
 import fx.redbox.common.Exception.UserNotFoundException;
 import fx.redbox.controller.donorCard.form.ReadAllDonorCardForm;
 import fx.redbox.controller.donorCard.form.ReadDonorCardForm;
+import fx.redbox.controller.donorCard.form.RedBoxDashboardInfo;
 import fx.redbox.entity.donorCards.DonorCard;
 import fx.redbox.entity.users.User;
 import fx.redbox.repository.donorCard.DonorCardRepository;
 import fx.redbox.repository.user.UserRepository;
-import fx.redbox.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -70,6 +70,25 @@ public class DonorCardServiceImpl implements DonorCardService{
         return convertToReadAllDonorCardFormList(donorCards);
     }
 
+    @Override
+    public RedBoxDashboardInfo readRedBoxDashboard(User user) {
+        // 사용자 존재 여부 확인
+        userRepository.findByUserId(user.getUserId()).orElseThrow(UserNotFoundException::new);
+
+        // 사용자 ID에 해당하는 헌혈증 수 카운트
+        int totalDonorCards = donorCardRepository.countDonorCardByUserId(1L);
+        int userDonorCards = donorCardRepository.countDonorCardByUserId(user.getUserId());
+
+        // 기여도 계산
+        double contributionRate;
+        if(totalDonorCards > 0) {
+            contributionRate = (double) userDonorCards / totalDonorCards * 100;
+        } else {
+            contributionRate = 0;
+        }
+
+        return new RedBoxDashboardInfo(totalDonorCards, userDonorCards, contributionRate);
+    }
 
     public List<ReadAllDonorCardForm> convertToReadAllDonorCardFormList(List<DonorCard> donorCards) {
         return donorCards.stream().map(donorCard -> {
